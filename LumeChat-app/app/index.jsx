558f0ce23dc4bfacbf299b9fb38a01cd8ebc30a5
Md Didarul Islam <t3ismd04@students.oamk.fi>
2025-03-24 22:41:54 +0200

    Refactor Firebase configuration and authentication setup; remove unused reducer file and update dependencies

--- LumeChat-app/app/index.jsx
+++ LumeChat-app/app/index.jsx
@@ -1,4 +1,3 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider } from "react-redux";
--- LumeChat-app/config/firebase.config.js
+++ LumeChat-app/config/firebase.config.js
@@ -1,20 +1,20 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const firebaseConfig = Constants.expoConfig.extra.firebaseConfig;
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
-const firebaseAuth = getAuth(app);
+// Initialize auth with AsyncStorage persistence
+const firebaseAuth = getApps.length > 0 
+  ? getAuth(app)
+  : initializeAuth(app, {
+      persistence: getReactNativePersistence(AsyncStorage)
+    });
+
 const firestoreDB = getFirestore(app);
 
 export { app, firebaseAuth, firestoreDB };
