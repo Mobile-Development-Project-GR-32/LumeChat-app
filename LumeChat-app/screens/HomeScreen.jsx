52568da109784d4095860188a14e7d3de8296230
rasmus-kaikkonen <t3kara00@students.oamk.fi>
2025-04-02 21:06:52 +0300

    Update HomeScreen.jsx

--- LumeChat-app/screens/HomeScreen.jsx
+++ LumeChat-app/screens/HomeScreen.jsx
@@ -1,21 +1,13 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
   const user = useSelector((state) => state.user)
-
-  const navigation = useNavigation()
 console.log("Logged User : ", user);
   return (
     <View>
       <Text>Home Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("ChatScreen")}>
-        <Text className="text-discord-link text-sm">
-            Test chat
-        </Text>
-      </TouchableOpacity>
     </View>
   )
 }
