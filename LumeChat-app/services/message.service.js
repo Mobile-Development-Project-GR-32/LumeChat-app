4b9a8e9e76948a99b6949d35500e85a48ca2230f
Rasmus Kaikkonen <t3kara00@students.oamk.fi>
2025-04-16 16:43:35 +0300

    Added styles to chat screen and separated channel and direct messaging
    Also added dependencies for GetStream.io and video/audio call feature.

--- LumeChat-app/services/message.service.js
+++ LumeChat-app/services/message.service.js
@@ -31,6 +31,47 @@ export const messageService = {
         }
     },
 
+    postDirectMessage: async (userId, messageData) => {
+        try {
+            console.log('Posting direct message data:', messageData)
+
+            const response = await fetch(`${API_URL}/messages/post-direct-message`, {
+                method: 'POST',
+                headers: messageService.getHeaders(userId),
+                body: JSON.stringify(messageData)
+            });
+
+            if(!response.ok) {
+                const error = await response.json();
+                throw new Error(error.error || 'Failed to post direct message');
+            }
+
+            return response.json()
+        } catch (error) {
+            console.error('Direct message posting error:', error);
+            throw error;
+        }
+    },
+
+    deleteDirectMessage: async (userId, messageId) => {
+        try {
+            const response = await fetch(`${API_URL}/messages/direct-messages/delete/${messageId}`, {
+                method: 'DELETE',
+                headers: messageService.getHeaders(userId)
+            });
+
+            if(!response.ok) {
+                const error = await response.json();
+                throw new Error(error.error || 'Failed to delete message');
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error('Message deletion error:', error);
+            throw error
+        }
+    },
+
     deleteMessage: async (userId, messageId) => {
         try {
             const response = await fetch(`${API_URL}/messages/delete/${messageId}`, {
@@ -53,6 +94,21 @@ export const messageService = {
     getMessagesByChannelName: async (channelName) => {
         try {
             const response = await fetch(`${API_URL}/messages/get-messages/${channelName}`)
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch messages');
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error('Messages fetch error:', error);
+            throw error;
+        }
+    },
+
+    getDirectMessages: async (userId, receiverId) => {
+        try {
+            const response = await fetch(`${API_URL}/direct-messages/get-messages/${userId}/${receiverId}`)
 
             if (!response.ok) {
                 throw new Error('Failed to fetch messages');
